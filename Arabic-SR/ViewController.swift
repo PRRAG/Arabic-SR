5e79fb5159373adf72806d022f782a5fc281e927
Pavly <pavlyremon@gmail.com>
2019-05-11 14:42:07 +0200

    Initial Commit

---
+++ Arabic-SR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arabic-SR
+//
+//  Created by Pavly Remon on 5/11/19.
+//  Copyright © 2019 Pavly Remon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
